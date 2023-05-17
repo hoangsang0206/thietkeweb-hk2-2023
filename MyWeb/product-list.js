9baa12651115d1122c656097db575e8537bc171f
Sang <lehoangsang02062003@gmail.com>
2023-05-17 17:32:18 +0700

    Update - 17/05/2023 - 17:32 PM

--- MyWeb/product-list.js
+++ MyWeb/product-list.js
@@ -2,8 +2,8 @@ const pageNumberBtn = document.querySelectorAll('.page-number-btn');
 
 pageNumberBtn.forEach((pageBtn1) => {
     pageBtn1.addEventListener('click', () => {
-        pageNumberBtn.forEach((pageBtn2) => {
-            pageBtn2.classList.remove('btn-active');
+        pageNumberBtn.forEach((pageBtn1) => {
+            pageBtn1.classList.remove('btn-active');
         })
         pageBtn1.classList.add('btn-active');
     })
