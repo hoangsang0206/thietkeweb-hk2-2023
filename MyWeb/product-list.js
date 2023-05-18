872b3cf74b8f5e52fa8d5e587635db227e1962f6
Sang <lehoangsang02062003@gmail.com>
2023-05-18 17:27:00 +0700

    Update - 18/05/2023 - 17:26 PM

--- MyWeb/product-list.js
+++ MyWeb/product-list.js
@@ -1,3 +1,34 @@
+//Filter
+const filterItem = document.querySelectorAll('.filter-item');
+const productListOverLay = document.querySelector('.product-list-overlay');
+
+filterItem.forEach((filterI) => {
+    const filterBtn = filterI.querySelector('.filter-btn');
+    const filterOption = filterI.querySelector('.filter-option');
+    const closeFilterOption = filterI.querySelector('.close-filter-option-btn');
+
+    filterBtn.addEventListener('click', () => {
+        filterOption.classList.toggle('show-filter-option');
+        filterI.classList.toggle('filter-item-zIndex');
+
+        closeFilterOption.addEventListener('click', () => {
+            filterOption.classList.remove('show-filter-option');
+            filterI.classList.remove('filter-item-zIndex');
+            productListOverLay.classList.remove('show-product-list-overlay');
+        })
+
+        productListOverLay.classList.toggle('show-product-list-overlay');
+    });
+
+    productListOverLay.addEventListener('click', () => {
+        filterOption.classList.remove('show-filter-option');
+        filterI.classList.remove('filter-item-zIndex');
+        productListOverLay.classList.remove('show-product-list-overlay');
+    })
+
+})
+
+//Page number ------------------------------------------
 const pageNumberBtn = document.querySelectorAll('.page-number-btn');
 
 pageNumberBtn.forEach((pageBtn1) => {
@@ -7,4 +38,4 @@ pageNumberBtn.forEach((pageBtn1) => {
         })
         pageBtn1.classList.add('btn-active');
     })
-});
+})
